c0e406255cedfe8c63398a809d594dbf8edf2f3a
marvelxcodes <ramakrishnan0046@gmail.com>
2023-03-12 09:41:17 +0530

    Added Type for API Context

--- utils/types.ts
+++ utils/types.ts
@@ -1,7 +1,11 @@
-export type ProjectType = {
+export interface ProjectType {
 	id: string;
 	userId: string;
 	name: string;
 	size: string;
 	properties: {};
-};
+}
+
+export interface NextContext {
+	params: { [key: string]: string };
+}
