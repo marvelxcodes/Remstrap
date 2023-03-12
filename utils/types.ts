29a6ad9d63c2d6f199ebc821047b611eca8ff1a0
marvelxcodes <ramakrishnan0046@gmail.com>
2023-03-12 09:44:21 +0530

    Undo last commit

--- utils/types.ts
+++ utils/types.ts
@@ -1,11 +1,7 @@
-export interface ProjectType {
+export type ProjectType = {
 	id: string;
 	userId: string;
 	name: string;
 	size: string;
 	properties: {};
-}
-
-export interface NextContext {
-	params: { [key: string]: string };
-}
+};
