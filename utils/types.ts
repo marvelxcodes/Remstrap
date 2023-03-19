383b453bb74604b9fabe0f30b1496565322439ca
marvelxcodes <ramakrishnan0046@gmail.com>
2023-03-19 19:46:51 +0530

    Added ProjectType

--- utils/types.ts
+++ utils/types.ts
@@ -1,9 +1,16 @@
-export type ProjectType = {
+export type ProjectsType = {
 	id: string;
 	name: string;
 	size: string;
 	image: string;
 	createdOn: Date;
+};
+
+export type ProjectType = {
+	id: string;
+	name: string;
+	size: string;
+	properties: string;
 };
 
 export type NextContext = {
