544969dd9d4319a7f2061c4cb6046ae7b202e9da
marvelxcodes <ramakrishnan0046@gmail.com>
2023-03-19 11:49:58 +0530

    Made data type explicit

--- slices/Modal.ts
+++ slices/Modal.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 type ModalStateType = {
 	currentModal: string;
-	data: {};
+	data: any;
 };
 
 const initialState: ModalStateType = {
