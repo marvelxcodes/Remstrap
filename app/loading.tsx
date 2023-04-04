1b8d76bda2b4f76d638b995be6a6e00f5154354a
marvelxcodes <ramakrishnan0046@gmail.com>
2023-04-04 23:06:35 +0530

    Added Loading Modal

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,11 +2,7 @@ import styles from './loading.module.scss';
 import Logo from '@/assets/Logo.svg';
 
 const loading = () => {
-	return (
-		<div className={styles.container}>
-			<Logo />
-		</div>
-	);
+	return <div className={styles.container}>{/* <Logo /> */}</div>;
 };
 
 export default loading;
