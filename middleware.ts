91986feb279a99db5417e40c48d2987592e65a87
marvelxcodes <ramakrishnan0046@gmail.com>
2023-04-07 23:22:02 +0530

    Removed unnecessary public paths

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ['/', '/sign-in*', '/sign-up*'];
+const publicPaths = ['/', '/sign-in*'];
 
 const isPublic = (path: string) => {
 	return publicPaths.find((x) =>
@@ -15,6 +15,7 @@ export default withClerkMiddleware((request: NextRequest) => {
 	if (isPublic(request.nextUrl.pathname)) {
 		return NextResponse.next();
 	}
+
 	// if the user is not signed in redirect them to the sign in page.
 	const { userId } = getAuth(request);
 	if (!userId) {
