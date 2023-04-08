41b73d3bf8fcef4eeccdf732e45665f4a2e56ffb
marvelxcodes <ramakrishnan0046@gmail.com>
2023-04-08 10:03:41 +0530

    Removed Hook: useDebounce

--- hooks/useDebounce.ts
+++
@@ -1,9 +0,0 @@
-export default function debounce(fn: any, delay: number) {
-	let timerId: any;
-	return (...args: any[]) => {
-		clearTimeout(timerId);
-		timerId = setTimeout(() => {
-			fn(...args);
-		}, delay);
-	};
-}
